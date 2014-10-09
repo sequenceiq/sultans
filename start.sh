99d82812fa6ac0a8358f6b1e0f53adf889830719
Marton Sereg <marton.sereg@sequenceiq.com>
2014-10-09 16:56:01 +0200

    fixed URL

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 : ${SL_ZIP:=master}
 
-cd /sultans && curl -LO $SL_ZIP && unzip /sultans/$SL_ZIP && cd /sultans/sultans-$SL_ZIP && npm install && node server.js
+SL_URL=https://github.com/sequenceiq/sultans/archive/$SL_ZIP.zip
+
+cd /sultans && curl -LO $SL_URL && unzip /sultans/$SL_ZIP && cd /sultans/sultans-$SL_ZIP && npm install && node server.js
