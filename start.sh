3e33ca807b92b90d34e1f7cfa462951fcec72085
Marton Sereg <marton.sereg@sequenceiq.com>
2014-10-09 17:07:16 +0200

    changed server.js to main.js

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 
 SL_URL=https://github.com/sequenceiq/sultans/archive/$SL_ZIP.zip
 
-cd /sultans && curl -LO $SL_URL && unzip /sultans/$SL_ZIP && cd /sultans/sultans-$SL_ZIP && npm install && node server.js
+cd /sultans && curl -LO $SL_URL && unzip /sultans/$SL_ZIP && cd /sultans/sultans-$SL_ZIP && npm install && node main.js
